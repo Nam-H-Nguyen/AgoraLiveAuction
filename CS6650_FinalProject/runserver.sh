81e3133cda12cafcf970464eb81d9466f4205138
WeUp-Team <nguyenbydoing@gmail.com>
2020-04-24 19:49:38 -0700

    Fix head issue for runserver.sh

--- CS6650_FinalProject/runserver.sh
+++ CS6650_FinalProject/runserver.sh
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
 #!/usr/bin/env bash
 find ./src -name "*.java" | xargs javac
 java -cp ./src server.AuctionServlet
-=======
-find ./src -name "*.java" | xargs javac
-java -cp ./src server.AuctionServlet
->>>>>>> d887371a31b523c9144872d7fae6d41e05db8093
